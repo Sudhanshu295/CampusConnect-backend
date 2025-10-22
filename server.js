f9f8e93e1399547b8e7ac882f9e377c94f1afa9f
Sudhanshu Jha <jhasudhanshu913@gmail.com>
2025-10-22 19:06:42 +0530

    add /

--- server.js
+++ server.js
@@ -150,6 +150,11 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Something went wrong!' });
 });
 
+app.get('/', (req, res) => {
+  res.send('CampusConnect backend is running!');
+});
+
+
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
